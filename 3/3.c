065a92fbb79ee2577e2c0ba8eff8f3209cdc2348
rojo <rohit.joshi@i2e1.com>
2018-12-18 10:48:25 +0530

    a bad solution to problem 3

---
+++ 3/3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int largestPrime(long int number){
+    int largest=2;
+    while(number>largest){
+        if(number%largest==0){
+            number=number/2;
+            largest=2;
+        }else
+        {
+            largest++;
+        }
+    }
+    return largest;
+}
+
+int main(void) {
+    //this takes a lot of time, i need to brush up my maths i guess 
+	long int num=600851475143;
+   
+	
+	printf("%d",largestPrime(num));
+	return 0;
+}
