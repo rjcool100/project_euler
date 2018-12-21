0595a0a367e626e0b7fe9707a771cf3e2e3f33b0
rojo <rohit.joshi@i2e1.com>
2018-12-21 10:15:33 +0530

    problem 6 solved

---
+++ 6/6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    //getting all highest powers of a prime before max and multiplying them
+    int n=100;
+    int sum=0,squaresum=0;
+    
+    sum=n*(n+1)/2;
+    sum*=sum;
+
+    squaresum=n*(n+1)*(2*n+1)/6;
+
+    
+
+    printf("difference: %d\n",abs(squaresum-sum));  
+
+  return 0;
+}
+
